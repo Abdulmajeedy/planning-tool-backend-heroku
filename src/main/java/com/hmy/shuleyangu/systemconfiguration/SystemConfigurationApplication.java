0ea9913799c5a06600a613636242e5ef867ff9dd
Moza Mohammed Said <mozasaid98@gmail.com>
2021-11-13 13:36:52 +0300

    first commit

---
+++ src/main/java/com/hmy/shuleyangu/systemconfiguration/SystemConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.hmy.shuleyangu.systemconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemConfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmy/shuleyangu/systemconfiguration/SystemConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmy.shuleyangu.systemconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
