a464af58ca203fdb730c6af996dbb83d546ce583
Abdulmajeedy <abdulmajeedhajji@gmail.com>
2022-06-23 03:16:54 +0300

    Relationship fixed

--- src/main/java/com/hmy/planning/systemconfiguration/service/loginService.java
+++ src/main/java/com/hmy/planning/systemconfiguration/service/loginService.java
@@ -1,98 +1,98 @@
-// package com.hmy.planning.systemconfiguration.service;
+package com.hmy.planning.systemconfiguration.service;
 
-// import java.util.ArrayList;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.Map;
-// import java.util.Optional;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
-// import org.modelmapper.ModelMapper;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.data.domain.PageRequest;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.stereotype.Service;
-// import org.springframework.web.server.ResponseStatusException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
-// import com.hmy.planning.systemconfiguration.dto.loginRequestDto;
-// import com.hmy.planning.systemconfiguration.dto.loginResponseDto;
-// import com.hmy.planning.systemconfiguration.models.login;
-// import com.hmy.planning.systemconfiguration.repository.LoginRepository;
+import com.hmy.planning.systemconfiguration.dto.loginRequestDto;
+import com.hmy.planning.systemconfiguration.dto.loginResponseDto;
+import com.hmy.planning.systemconfiguration.models.login;
+import com.hmy.planning.systemconfiguration.repository.LoginRepository;
 
-// import lombok.Data;
+import lombok.Data;
 
-// @Service
-// @Data
-// public class loginService {
+@Service
+@Data
+public class loginService {
 
-// @Autowired
-// private LoginRepository loginRepo;
-// private final ModelMapper modelmapper;
+    @Autowired
+    private LoginRepository loginRepo;
+    private final ModelMapper modelmapper;
 
-// public Optional<login> login(String email) {
-// return loginRepo.findByEmail(email);
-// }
+    public Optional<login> login(String email) {
+        return loginRepo.findByEmail(email);
+    }
 
-// public List<loginResponseDto> findAlllogin(int page, int size) {
-// PageRequest pageRequest = PageRequest.of(page, size);
-// List<login> logn = loginRepo.findAll(pageRequest).getContent();
-// List<loginResponseDto> loginDto = new ArrayList<>();
-// for (login lon : logn) {
-// loginResponseDto responseDto = modelmapper.map(lon, loginResponseDto.class);
-// responseDto.setRoleCode(lon.getRole().getRoleCode());
-// loginDto.add(responseDto);
-// }
-// return loginDto;
-// }
+    public List<loginResponseDto> findAlllogin(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        List<login> logn = loginRepo.findAll(pageRequest).getContent();
+        List<loginResponseDto> loginDto = new ArrayList<>();
+        for (login lon : logn) {
+            loginResponseDto responseDto = modelmapper.map(lon, loginResponseDto.class);
+            responseDto.setRoleCode(lon.getRole().getRoleCode());
+            loginDto.add(responseDto);
+        }
+        return loginDto;
+    }
 
-// public void deleteObjective(String LoginCode) {
+    public void deleteObjective(String LoginCode) {
 
-// loginRepo.deleteById(LoginCode);
-// }
+        loginRepo.deleteById(LoginCode);
+    }
 
-// public loginResponseDto getLoginById(String LoginCode) {
-// Optional<login> login = loginRepo.findById(LoginCode);
-// if (!login.isPresent()) {
-// throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-// "project with this code" + LoginCode + "is not Found");
-// } else {
-// login obj = login.get();
-// loginResponseDto responseDto = modelmapper.map(obj, loginResponseDto.class);
-// responseDto.setLoginCode(obj.getLoginCode());
-// return responseDto;
-// }
-// }
+    public loginResponseDto getLoginById(String LoginCode) {
+        Optional<login> login = loginRepo.findById(LoginCode);
+        if (!login.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "project with this code" + LoginCode + "is not Found");
+        } else {
+            login obj = login.get();
+            loginResponseDto responseDto = modelmapper.map(obj, loginResponseDto.class);
+            responseDto.setLoginCode(obj.getLoginCode());
+            return responseDto;
+        }
+    }
 
-// public Optional<login> getLoginCode(String loginCode) {
-// return loginRepo.findById(loginCode);
-// }
+    public Optional<login> getLoginCode(String loginCode) {
+        return loginRepo.findById(loginCode);
+    }
 
-// public Map<String, Boolean> updateStatus(String LoginCode) {
-// Optional<login> bp = loginRepo.findById(LoginCode);
-// if (!bp.isPresent()) {
-// throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-// }
-// if (bp.get().getStatus() == 1)
-// bp.get().setStatus(0);
-// else
-// bp.get().setStatus(1);
-// loginRepo.save(bp.get());
-// Map<String, Boolean> response = new HashMap<>();
-// response.put("response", Boolean.TRUE);
-// return response;
-// }
+    public Map<String, Boolean> updateStatus(String LoginCode) {
+        Optional<login> bp = loginRepo.findById(LoginCode);
+        if (!bp.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        if (bp.get().getStatus() == 1)
+            bp.get().setStatus(0);
+        else
+            bp.get().setStatus(1);
+        loginRepo.save(bp.get());
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("response", Boolean.TRUE);
+        return response;
+    }
 
-// public List<loginResponseDto> findAllLogin(int page, int size) {
-// PageRequest pageRequest = PageRequest.of(page, size);
-// List<login> obje = loginRepo.findAll(pageRequest).getContent();
-// List<loginResponseDto> objDto = new ArrayList<>();
-// for (login objective : obje) {
-// loginResponseDto responseDto = modelmapper.map(objective,
-// loginResponseDto.class);
-// responseDto.setRoleCode(objective.getRole().getRoleCode());
-// objDto.add(responseDto);
-// }
-// return objDto;
-// }
+    public List<loginResponseDto> findAllLogin(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        List<login> obje = loginRepo.findAll(pageRequest).getContent();
+        List<loginResponseDto> objDto = new ArrayList<>();
+        for (login objective : obje) {
+            loginResponseDto responseDto = modelmapper.map(objective,
+                    loginResponseDto.class);
+            responseDto.setRoleCode(objective.getRole().getRoleCode());
+            objDto.add(responseDto);
+        }
+        return objDto;
+    }
 
-// }
+}
--- src/main/java/com/hmy/planning/systemconfiguration/web/staffController.java
+++ src/main/java/com/hmy/planning/systemconfiguration/web/staffController.java
@@ -1,130 +1,129 @@
-// package com.hmy.planning.systemconfiguration.web;
+package com.hmy.planning.systemconfiguration.web;
 
-// import java.util.ArrayList;
-// import java.util.List;
-// import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-// import org.modelmapper.ModelMapper;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.data.domain.PageRequest;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.hmy.planning.systemconfiguration.dto.staffRequestDto;
-// import com.hmy.planning.systemconfiguration.dto.staffResponseDto;
-// import com.hmy.planning.systemconfiguration.models.staff;
-// import com.hmy.planning.systemconfiguration.service.staffService;
-// import com.hmy.planning.systemconfiguration.utils.ApiResponse;
-// import com.hmy.planning.systemconfiguration.web.api.staffApi;
+import com.hmy.planning.systemconfiguration.dto.staffRequestDto;
+import com.hmy.planning.systemconfiguration.dto.staffResponseDto;
+import com.hmy.planning.systemconfiguration.models.staff;
+import com.hmy.planning.systemconfiguration.service.staffService;
+import com.hmy.planning.systemconfiguration.utils.ApiResponse;
+import com.hmy.planning.systemconfiguration.web.api.staffApi;
 
-// @RestController
-// public class staffController implements staffApi {
+@RestController
+public class staffController implements staffApi {
 
-// private staffService staffServices;
-// private ModelMapper modelMapper;
+    private staffService staffServices;
+    private ModelMapper modelMapper;
 
-// @Autowired
-// public staffController(staffService staffServices, ModelMapper modelMapper) {
+    @Autowired
+    public staffController(staffService staffServices, ModelMapper modelMapper) {
 
-// this.staffServices = staffServices;
-// this.modelMapper = modelMapper;
-// }
+        this.staffServices = staffServices;
+        this.modelMapper = modelMapper;
+    }
 
-// @Override
-// public ResponseEntity<List<staffResponseDto>> getStaff(int page, int size) {
-// PageRequest pageRequest = PageRequest.of(page, size);
-// List<staff> staff = staffServices.findAllStaff(pageRequest);
-// List<staffResponseDto> actDto = new ArrayList<>();
-// for (staff st : staff) {
-// staffResponseDto responseDto = new staffResponseDto();
+    @Override
+    public ResponseEntity<List<staffResponseDto>> getStaff(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        List<staff> staff = staffServices.findAllStaff(pageRequest);
+        List<staffResponseDto> actDto = new ArrayList<>();
+        for (staff st : staff) {
+            staffResponseDto responseDto = new staffResponseDto();
 
-// responseDto.setStaffID(st.getStaffID());
-// responseDto.setFirstname(st.getFirstName());
-// responseDto.setMiddlename(st.getMiddleName());
-// responseDto.setLastname(st.getLastName());
-// responseDto.setEmail(st.getEmail());
-// responseDto.setPhone(st.getPhone());
-// responseDto.setStaffCode(st.getStaffCode());
-// responseDto.setOfficeID(st.getOrgStructure().getOfficeID());
-// responseDto.setStatus(st.getStatus());
-// responseDto.setStatus(st.getStatus());
-// responseDto.setCreatedDate(st.getCreatedDate());
-// responseDto.setCreatedBy(st.getCreatedBy());
-// responseDto.setModifiedDate(st.getModifiedDate());
-// responseDto.setModifiedBy(st.getModifiedBy());
-// actDto.add(responseDto);
-// }
-// return ResponseEntity.ok(actDto);
-// }
+            responseDto.setStaffID(st.getStaffID());
+            responseDto.setFirstname(st.getFirstName());
+            responseDto.setMiddlename(st.getMiddleName());
+            responseDto.setLastname(st.getLastName());
+            responseDto.setEmail(st.getEmail());
+            responseDto.setPhone(st.getPhone());
+            responseDto.setStaffCode(st.getStaffCode());
+            responseDto.setOfficeID(st.getOrgStructure().getOfficeID());
+            responseDto.setStatus(st.getStatus());
+            responseDto.setStatus(st.getStatus());
+            responseDto.setCreatedDate(st.getCreatedDate());
+            responseDto.setCreatedBy(st.getCreatedBy());
+            responseDto.setModifiedDate(st.getModifiedDate());
+            responseDto.setModifiedBy(st.getModifiedBy());
+            actDto.add(responseDto);
+        }
+        return ResponseEntity.ok(actDto);
+    }
 
-// @Override
-// public ResponseEntity<staffResponseDto> registerNewStaff(staffRequestDto
-// reqStaff) {
-// return staffServices.addNewStaff(reqStaff);
-// }
+    @Override
+    public ResponseEntity<staffResponseDto> registerNewStaff(staffRequestDto reqStaff) {
+        return staffServices.addNewStaff(reqStaff);
+    }
 
-// @Override
-// public ResponseEntity<staffResponseDto> getStaffById(String staffID) {
-// Optional<staff> orgStr = staffServices.getStaffById(staffID);
-// if (!orgStr.isPresent()) {
-// return new ResponseEntity(ApiResponse.error("Invalid Activity Code", null),
-// HttpStatus.NOT_FOUND);
-// }
-// staff bg = orgStr.get();
-// staffResponseDto responseDto = new staffResponseDto();
+    @Override
+    public ResponseEntity<staffResponseDto> getStaffById(String staffID) {
+        Optional<staff> orgStr = staffServices.getStaffById(staffID);
+        if (!orgStr.isPresent()) {
+            return new ResponseEntity(ApiResponse.error("Invalid Activity Code", null),
+                    HttpStatus.NOT_FOUND);
+        }
+        staff bg = orgStr.get();
+        staffResponseDto responseDto = new staffResponseDto();
 
-// responseDto.setStaffID(bg.getStaffID());
-// responseDto.setFirstname(bg.getFirstName());
-// responseDto.setMiddlename(bg.getMiddleName());
-// responseDto.setLastname(bg.getLastName());
-// responseDto.setEmail(bg.getEmail());
-// responseDto.setPhone(bg.getPhone());
-// responseDto.setStaffCode(bg.getStaffCode());
-// responseDto.setStatus(bg.getStatus());
-// responseDto.setCreatedDate(bg.getCreatedDate());
-// responseDto.setOfficeID(bg.getOrgStructure().getOfficeID());
-// responseDto.setCreatedBy(bg.getCreatedBy());
-// responseDto.setModifiedDate(bg.getModifiedDate());
-// responseDto.setModifiedBy(bg.getModifiedBy());
+        responseDto.setStaffID(bg.getStaffID());
+        responseDto.setFirstname(bg.getFirstName());
+        responseDto.setMiddlename(bg.getMiddleName());
+        responseDto.setLastname(bg.getLastName());
+        responseDto.setEmail(bg.getEmail());
+        responseDto.setPhone(bg.getPhone());
+        responseDto.setStaffCode(bg.getStaffCode());
+        responseDto.setStatus(bg.getStatus());
+        responseDto.setCreatedDate(bg.getCreatedDate());
+        responseDto.setOfficeID(bg.getOrgStructure().getOfficeID());
+        responseDto.setCreatedBy(bg.getCreatedBy());
+        responseDto.setModifiedDate(bg.getModifiedDate());
+        responseDto.setModifiedBy(bg.getModifiedBy());
 
-// return ResponseEntity.ok(responseDto);
-// }
+        return ResponseEntity.ok(responseDto);
+    }
 
-// @Override
-// public void deleteById(String staffID) {
-// staffServices.deleteStaff(staffID);
+    @Override
+    public void deleteById(String staffID) {
+        staffServices.deleteStaff(staffID);
 
-// }
+    }
 
-// @Override
-// public ResponseEntity updateStaff(String staffID, staff reqStaff) {
-// Optional<staff> budPer = staffServices.getStaffById(staffID);
-// staffServices.updateStaff(staffID, reqStaff);
-// staff stf = budPer.get();
-// staffResponseDto responseDto = new staffResponseDto();
-// responseDto.setStaffID(stf.getStaffID());
-// responseDto.setFirstname(stf.getFirstName());
-// responseDto.setMiddlename(stf.getMiddleName());
-// responseDto.setLastname(stf.getLastName());
-// responseDto.setEmail(stf.getEmail());
-// responseDto.setPhone(stf.getPhone());
-// responseDto.setStaffCode(stf.getStaffCode());
-// responseDto.setCreatedDate(stf.getCreatedDate());
-// responseDto.setCreatedBy(stf.getCreatedBy());
-// responseDto.setModifiedDate(stf.getModifiedDate());
-// responseDto.setModifiedBy(stf.getModifiedBy());
-// return ResponseEntity.ok(responseDto);
-// }
+    @Override
+    public ResponseEntity updateStaff(String staffID, staff reqStaff) {
+        Optional<staff> budPer = staffServices.getStaffById(staffID);
+        staffServices.updateStaff(staffID, reqStaff);
+        staff stf = budPer.get();
+        staffResponseDto responseDto = new staffResponseDto();
+        responseDto.setStaffID(stf.getStaffID());
+        responseDto.setFirstname(stf.getFirstName());
+        responseDto.setMiddlename(stf.getMiddleName());
+        responseDto.setLastname(stf.getLastName());
+        responseDto.setEmail(stf.getEmail());
+        responseDto.setPhone(stf.getPhone());
+        responseDto.setStaffCode(stf.getStaffCode());
+        responseDto.setCreatedDate(stf.getCreatedDate());
+        responseDto.setCreatedBy(stf.getCreatedBy());
+        responseDto.setModifiedDate(stf.getModifiedDate());
+        responseDto.setModifiedBy(stf.getModifiedBy());
+        return ResponseEntity.ok(responseDto);
+    }
 
-// @Override
-// public ResponseEntity updateStatus(String staffID) {
-// return ResponseEntity.ok().body(staffServices.updateStatus(staffID));
-// }
+    @Override
+    public ResponseEntity updateStatus(String staffID) {
+        return ResponseEntity.ok().body(staffServices.updateStatus(staffID));
+    }
 
-// @Override
-// public ResponseEntity Count() {
-// return ResponseEntity.ok().body(staffServices.CountStaff());
-// }
+    @Override
+    public ResponseEntity Count() {
+        return ResponseEntity.ok().body(staffServices.CountStaff());
+    }
 
-// }
+}
