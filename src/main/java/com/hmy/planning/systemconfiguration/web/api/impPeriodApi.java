d40ea6bc6934ab9e8bee450d5430f400271d98a9
Abdulmajeedy <abdulmajeedhajji@gmail.com>
2022-06-25 19:06:19 +0300

    IMP eND pOINT aDDED

--- src/main/java/com/hmy/planning/systemconfiguration/web/api/impPeriodApi.java
+++ src/main/java/com/hmy/planning/systemconfiguration/web/api/impPeriodApi.java
@@ -10,42 +10,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.hmy.planning.systemconfiguration.dto.impPeriodRequestDto;
 import com.hmy.planning.systemconfiguration.dto.impPeriodResponseDto;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @CrossOrigin
 @RequestMapping("/impPeriod")
 public interface impPeriodApi {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<List<impPeriodResponseDto>> getimpPeriode(
-            @RequestParam(defaultValue = "0", required = false) int page,
-            @RequestParam(defaultValue = "10", required = false) int size);
+        @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+        public ResponseEntity<List<impPeriodResponseDto>> getimpPeriode(
+                        @RequestParam(defaultValue = "0", required = false) int page,
+                        @RequestParam(defaultValue = "10", required = false) int size);
 
-    @RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<impPeriodResponseDto> registerNewimpPeriode(
-            @RequestBody impPeriodRequestDto impPeriode);
+        @RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json")
+        public ResponseEntity<impPeriodResponseDto> registerNewimpPeriode(
+                        @RequestBody impPeriodRequestDto impPeriode);
 
-    @RequestMapping(value = "/{impPeriodCode}", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<impPeriodResponseDto> getimpPeriodeById(@PathVariable String impPeriodCode);
+        @RequestMapping(value = "/{impPeriodCode}", method = RequestMethod.GET, produces = "application/json")
+        public ResponseEntity<impPeriodResponseDto> getimpPeriodeById(@PathVariable String impPeriodCode);
 
-    @DeleteMapping(path = "/{impPeriodCode}")
-    @ResponseStatus(code = HttpStatus.OK, reason = "impPeriodCode   Deleted")
-    public void deleteById(@PathVariable("impPeriodCode") String impPeriodCode);
+        @DeleteMapping(path = "/{impPeriodCode}")
+        @ResponseStatus(code = HttpStatus.OK, reason = "impPeriodCode   Deleted")
+        public void deleteById(@PathVariable("impPeriodCode") String impPeriodCode);
 
-    @DeleteMapping(path = "delete/")
-    @ResponseStatus(code = HttpStatus.OK, reason = "impPeriodCode   Deleted")
-    public void deleteAll();
+        @DeleteMapping(path = "delete/")
+        @ResponseStatus(code = HttpStatus.OK, reason = "impPeriodCode   Deleted")
+        public void deleteAll();
 
-    @RequestMapping(value = "/{impPeriodCode}", method = RequestMethod.PUT, produces = "application/json", consumes = "application/json")
-    public ResponseEntity updateimpPeriode(@PathVariable("objectiveCode") String impPeriodCode,
-            @RequestBody impPeriodRequestDto objectives);
+        @RequestMapping(value = "/{impPeriodCode}", method = RequestMethod.PUT, produces = "application/json", consumes = "application/json")
+        public ResponseEntity updateimpPeriode(@PathVariable("objectiveCode") String impPeriodCode,
+                        @RequestBody impPeriodRequestDto objectives);
 
-    @RequestMapping(value = "updateStatus/{impPeriodCode}", method = RequestMethod.GET)
-    public ResponseEntity updateStatus(@PathVariable("impPeriodCode") String impPeriodCode);
+        @RequestMapping(value = "updateStatus/{impPeriodCode}", method = RequestMethod.GET)
+        public ResponseEntity updateStatus(@PathVariable("impPeriodCode") String impPeriodCode);
 
 }
