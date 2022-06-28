fe9eb848a51c56179be10b76945226327bceb839
Abdulmajeedy <abdulmajeedhajji@gmail.com>
2022-06-28 03:11:35 +0300

    Budget Endpoint Added

--- src/main/java/com/hmy/planning/systemconfiguration/service/budgetService.java
+++ src/main/java/com/hmy/planning/systemconfiguration/service/budgetService.java
@@ -55,7 +55,7 @@ public class budgetService {
         budgetRepo.save(budget);
 
         budgetResponseDto bu = modelmapper.map(budget, budgetResponseDto.class);
-        bu.setActivityCode(budget.getActivities().getActivityCode())
+        bu.setActivityCode(budget.getActivities().getActivityCode());
         return ResponseEntity.ok(bu);
 
     }
