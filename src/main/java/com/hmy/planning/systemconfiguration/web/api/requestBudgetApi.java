0abfb9d9616fa4b793784e9de680ffd2b3063709
Abdulmajeedy <abdulmajeedhajji@gmail.com>
2022-06-30 01:31:14 +0300

    New commits

--- src/main/java/com/hmy/planning/systemconfiguration/web/api/requestBudgetApi.java
+++ src/main/java/com/hmy/planning/systemconfiguration/web/api/requestBudgetApi.java
@@ -1,6 +1,6 @@
 package com.hmy.planning.systemconfiguration.web.api;
 
-import java.util.*;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,34 +17,31 @@ import com.hmy.planning.systemconfiguration.dto.requestBudgetRequestDto;
 import com.hmy.planning.systemconfiguration.dto.requestBudgetResponseDto;
 
 @CrossOrigin
-@RequestMapping("/requestBudget")
+@RequestMapping("/budget-Period")
 public interface requestBudgetApi {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<List<requestBudgetResponseDto>> getRequestBudge(
-            @RequestParam(defaultValue = "0", required = false) int page,
-            @RequestParam(defaultValue = "10", required = false) int size);
+        @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+        public ResponseEntity<List<requestBudgetResponseDto>> getrequestBudget(
+                        @RequestParam(defaultValue = "0", required = false) int page,
+                        @RequestParam(defaultValue = "10", required = false) int size);
 
-    @RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<requestBudgetResponseDto> registerRequestBudge(
-            @RequestBody requestBudgetRequestDto requestBudget);
+        @RequestMapping(value = "/", method = RequestMethod.POST, produces = "application/json")
+        public ResponseEntity<requestBudgetResponseDto> registerNewrequestBudget(
+                        @RequestBody requestBudgetRequestDto budgetPeriod);
 
-    @RequestMapping(value = "/{impPeriodCode}", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<requestBudgetResponseDto> getRequestBudgetById(@PathVariable String impPeriodCode);
+        @RequestMapping(value = "/{requestBudgetCode}", method = RequestMethod.GET, produces = "application/json")
+        public ResponseEntity<requestBudgetResponseDto> getrequestBudgetById(
+                        @PathVariable("requestBudgetCode") String requestBudgetCode);
 
-    @DeleteMapping(path = "/{requestBudgetCode}")
-    @ResponseStatus(code = HttpStatus.OK, reason = "impPeriodCode   Deleted")
-    public void deleteById(@PathVariable("requestBudgetCode") String impPeriodCode);
+        @DeleteMapping(path = "/{requestBudgetCode}")
+        @ResponseStatus(code = HttpStatus.OK, reason = "Bugeting Period Deleted")
+        public void deleteById(@PathVariable("requestBudgetCode") String requestBudgetCode);
 
-    @DeleteMapping(path = "delete/")
-    @ResponseStatus(code = HttpStatus.OK, reason = "requestBudgetCode   Deleted")
-    public void deleteAll();
+        @RequestMapping(value = "/{requestBudgetCode}", method = RequestMethod.PUT, produces = "application/json", consumes = "application/json")
+        public ResponseEntity updaterequestBudget(@PathVariable("requestBudgetCode") String requestBudgetCode,
+                        @RequestBody requestBudgetRequestDto budgetPeriod);
 
-    @RequestMapping(value = "/{requestBudgetCode}", method = RequestMethod.PUT, produces = "application/json", consumes = "application/json")
-    public ResponseEntity updateRequestBudge(@PathVariable("requestBudgetCode") String impPeriodCode,
-            @RequestBody requestBudgetRequestDto requestBudget);
-
-    @RequestMapping(value = "updateStatus/{requestBudgetCode}", method = RequestMethod.GET)
-    public ResponseEntity updateStatus(@PathVariable("requestBudgetCode") String impPeriodCode);
+        @RequestMapping(value = "updateStatus/{requestBudgetCode}", method = RequestMethod.GET)
+        public ResponseEntity updateStatus(@PathVariable("requestBudgetCode") String requestBudgetCode);
 
 }
--- src/main/java/com/hmy/planning/systemconfiguration/web/requestBudgetController.java
+++ src/main/java/com/hmy/planning/systemconfiguration/web/requestBudgetController.java
@@ -1,6 +1,6 @@
 package com.hmy.planning.systemconfiguration.web;
 
-import java.util.*;
+import java.util.List;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,63 +8,57 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hmy.planning.systemconfiguration.dto.requestBudgetRequestDto;
-import com.hmy.planning.systemconfiguration.dto.requestBudgetResponseDto;
-import com.hmy.planning.systemconfiguration.repository.RoleRepository;
 import com.hmy.planning.systemconfiguration.service.requestBudgetService;
 import com.hmy.planning.systemconfiguration.web.api.requestBudgetApi;
 
 @RestController
+
 public class requestBudgetController implements requestBudgetApi {
 
-    private requestBudgetService requestBudgetService;
-    private RoleRepository roleRepo;
     private ModelMapper modelMapper;
+    private requestBudgetService reqBudgetService;
 
     @Autowired
-    public requestBudgetController(requestBudgetService requestBudgetService, ModelMapper modelMapper) {
+    public requestBudgetController(requestBudgetService reqBudgetService, ModelMapper modelMapper) {
 
-        this.requestBudgetService = requestBudgetService;
+        this.reqBudgetService = reqBudgetService;
         this.modelMapper = modelMapper;
     }
 
-    public ResponseEntity<requestBudgetResponseDto> registerRequestBudget(requestBudgetRequestDto reqRequestBudget) {
-        return requestBudgetService.addNewRequestBudget(reqRequestBudget);
+    @Override
+    public ResponseEntity<List<com.hmy.planning.systemconfiguration.dto.requestBudgetResponseDto>> getrequestBudget(
+            int page, int size) {
+        return ResponseEntity.ok(reqBudgetService.findAllrequestBudget(page, size));
     }
 
     @Override
-    public ResponseEntity<requestBudgetResponseDto> getRequestBudgetById(String requestBudgetCode) {
-        return ResponseEntity.ok(requestBudgetService.getRequestBudgetById(requestBudgetCode));
+    public ResponseEntity<com.hmy.planning.systemconfiguration.dto.requestBudgetResponseDto> registerNewrequestBudget(
+            requestBudgetRequestDto budgetPeriod) {
+        return reqBudgetService.addNewrequestBudget(budgetPeriod);
     }
 
+    @Override
+    public ResponseEntity<com.hmy.planning.systemconfiguration.dto.requestBudgetResponseDto> getrequestBudgetById(
+            String requestBudgetCode) {
+        return ResponseEntity.ok(reqBudgetService.getrequestBudgetById(requestBudgetCode));
+
+    }
+
+    @Override
     public void deleteById(String requestBudgetCode) {
-        requestBudgetService.deleteRequestBudget(requestBudgetCode);
-    }
-
-    @Override
-    public ResponseEntity updateStatus(String roleCode) {
-        return ResponseEntity.ok().body(requestBudgetService.updateStatus(roleCode));
-    }
-
-    @Override
-    public ResponseEntity<List<requestBudgetResponseDto>> getRequestBudge(int page, int size) {
-        return ResponseEntity.ok(requestBudgetService.findAllRequestBudget(page, size));
+        reqBudgetService.deleterequestBudget(requestBudgetCode);
 
     }
 
     @Override
-    public void deleteAll() {
-        requestBudgetService.deleteAllRequestBudget();
+    public ResponseEntity updaterequestBudget(String requestBudgetCode, requestBudgetRequestDto budgetPeriod) {
+        return ResponseEntity.ok(reqBudgetService.updaterequestBudget(requestBudgetCode, budgetPeriod));
     }
 
     @Override
-    public ResponseEntity<requestBudgetResponseDto> registerRequestBudge(requestBudgetRequestDto requestBudget) {
-        return requestBudgetService.addNewRequestBudget(requestBudget);
+    public ResponseEntity updateStatus(String requestBudgetCode) {
+        return ResponseEntity.ok().body(reqBudgetService.updateStatus(requestBudgetCode));
 
     }
 
-    @Override
-    public ResponseEntity updateRequestBudge(String impPeriodCode, requestBudgetRequestDto requestBudget) {
-        // TODO Auto-generated method stub
-        return null;
-    }
 }
